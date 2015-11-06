72eb26cac07286b2e71b9cc31f205e29d8258d0c
swiercc <swiercc62@gmail.com>
2015-11-06 11:09:34 -0500

    ho
    slllll

--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,6 +9,29 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+
+            double tax;
+
+            double net;
+
+            Console.WriteLine("Hourly Pay rate: ");
+            double hourlyRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Hours Worked: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            double gross = hourlyRate * hours;
+
+            if (gross <= 300.00)
+                tax = gross * .10;
+            else
+                tax = gross * .12;
+
+            net = gross - tax;
+
+            Console.WriteLine("Gross Pay: {0}   Witholding Tax: {1}   Net Pay: {2}"), gross , tax , net;
+    
+
         }
     }
 }
